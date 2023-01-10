7d08b6416c96621b16347e2261b4e2f95bbfd5b6
Ochi Daniel <dtonye338@gmail.com>
2023-01-10 09:45:30 +0100

    Finall

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -9,7 +9,6 @@ import 'swiper/css/pagination';
 import User from './images/avatar-anisha.png';
 import User2 from './images/avatar-ali.png';
 import User3  from './images/avatar-richard.png';
-
 const Testimonials = () => {
   return (
     <div>
@@ -25,8 +24,8 @@ const Testimonials = () => {
                    <img src={User} class="-mt-14 w-16" alt="userImg" />
                    <h2 class=" px-4 text-2xl text-darkBlue font-bold ">Anisha Vasquez
                    </h2>
-                   <p class="text-left text-sm text-grayishBlue md:text-center">“Their software allows us to track, manage and collaborate on our projects 
-  from anywhere. It keeps the whole team in-sync without being intrusive.”</p>
+                   <p class="text-left text-sm text-grayishBlue md:text-center">“Set internal delivery estimates and track progress toward company
+                       goals. Our customisable dashboard helps you build out” </p>
                </div>
                
                  {/* <!--USER 2--> */}
@@ -34,17 +33,15 @@ const Testimonials = () => {
                    <img src={User2} class="-mt-14 w-16" alt="userImg" />
                    <h2 class=" px-4 text-2xl text-darkBlue font-bold ">Chisom Theckla
                    </h2>
-                   <p class="text-left text-sm text-grayishBlue md:text-center"> “We have been able to cancel so many other subscriptions since using 
-  Manage. There is no more cross-channel confusion and everyone is much 
-  more focused.” </p>
+                   <p class="text-left text-sm text-grayishBlue md:text-center">“We have been able to cancel so many other subscriptions since using 
+  Manage. There is no more cross-channel ” </p>
                </div>
                  {/* <!--USER 3--> */}
                  <div class="hidden flex flex-col items-center bg-gray-100 p-6 space-y-6 md:w-1/3 md:flex">
                    <img src={User3} class="-mt-14 w-16" alt="userImg" />
                    <h2 class=" px-4 text-2xl text-darkBlue font-bold ">Jameson Cloft
                    </h2>
-                   <p class="text-left text-grayishBlue text-sm md:text-center">“Manage allows us to provide structure and process. It keeps us organized 
-  and focused. I can’t stop recommending them to everyone I talk to!”</p>
+                   <p class="text-left text-grayishBlue text-sm md:text-center">“Their software allows us to track, manage and collaborate on our project from anywhere. It keeps the whole team in-sync without being intrusive.”</p>
                </div>
                
         </div>
@@ -55,15 +52,15 @@ const Testimonials = () => {
             pagination={{ clickable: true }}
             speed={500}
             slidesPerView={1}
-            className='myswiper container md:hidden '
-            >
+            className='myswiper container md:hidden '>
             <SwiperSlide className='swiperslide'>
                {/* <!--USER 1--> */}
                <div class="flex flex-col items-center bg-gray-100 p-6 px-8 space-y-6">
                    <img src={User} class="-mt-14 w-16" alt="userImg" />
                    <h2 class=" px-4 text-2xl text-darkBlue font-bold ">Anisha Vasquez
                    </h2>
-                   <p class="text-center text-sm text-grayishBlue md:text-center">“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”</p>
+                   <p class="text-center text-sm text-grayishBlue md:text-center">“Set internal delivery estimates and track progress toward company”
+                       goals. Our customisable dashboard helps you build out </p>
                </div>
             </SwiperSlide>
             <SwiperSlide className='swiperslide'>
@@ -72,7 +69,7 @@ const Testimonials = () => {
                    <img src={User2} class="-mt-14 w-16" alt="userImg" />
                    <h2 class=" px-4 text-2xl text-darkBlue font-bold ">Chisom Theckla
                    </h2>
-                   <p class="text-center text-sm text-grayishBlue md:text-center">“We have been able to cancel so many other subscriptions since using  Manage. There is no more cross-channel confusion and everyone is much more focused.” </p>
+                   <p class="text-center text-sm text-grayishBlue md:text-center">“We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel”</p>
                </div>
             </SwiperSlide>
             <SwiperSlide className='swiperslide'>
@@ -81,13 +78,13 @@ const Testimonials = () => {
                    <img src={User3} class="-mt-14 w-16" alt="userImg" />
                    <h2 class=" px-4 text-2xl text-darkBlue font-bold ">Jameson Cloft
                    </h2>
-                   <p class="text-center text-grayishBlue text-sm md:text-center"> “Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!” </p>
+                   <p class="text-center text-grayishBlue text-sm md:text-center">“Their software allows us to track, manage and collaborate on our project from anywhere. It keeps the whole team in-sync without being intrusive.”</p>
                </div>
             </SwiperSlide>
         </Swiper>
     </div>
     <div class="container mx-auto flex flex-col items-center mt-10 md:justify-center">
-       <a href="#" class="rounded-full bg-brightRed p-3 px-10  text-white font-sem-bold baseline" >Get Started</a>
+       <a href="#" class="rounded-full bg-brightRed p-3 px-10  text-white font-semi-bold baseline" >Get Started</a>
     </div>     
     
 
